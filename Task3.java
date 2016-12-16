be39a92ffd51da4397a40df1ac1fc5da94b30689
aniketh reddy <waraniketh1@gmail.com>
2016-12-16 07:28:53 +0530

    added a program

---
+++ Task3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Task3{
+public void static main(String args[])
+{
+int a,b,sum,product;
+a=Integer.pasreInt(args[0]);
+b=Integer.pasreInt(args[1]);
+sum=Integer.pasreInt(args[2);
+product=Integer.pasreInt(args[3]);
+sum=a+b;
+for(int i=0;i<5;i++){
+product=product+sum;
+}
+}
+
+}
